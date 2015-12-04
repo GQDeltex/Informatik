4b08a6bd9e19a621855353ef1cc6ab8dcbd423d9
peter <operitum@gmail.com>
2015-12-04 17:15:37 +0100

    Wuerfelspiel mit Grafik
    Habe das Würfelspiel mit einer "Grafischen oberfläche" versehen und die
    Spielmachanik leicht geaendert

--- Würfelspiel/wuerfelspiel.c
+++ Würfelspiel/wuerfelspiel.c
@@ -9,7 +9,13 @@ int main(int argc, char *argv[]) {
 	int zahl;
 	int zahl_summe;
 	unsigned char ja_nein;
-	
+	int error;
+	int win;
+	int eingabe;
+	int vorher;
+	int high;
+	int zaehler;
+		
 	printf("W""\x81""rfelspiel\n");
 	printf("Ziel: Moeglichst viele Punkt erreichen\n");
 	printf("Einmal w""\x81""rfeln: 1 bis maximal 6 Punkte\n");
@@ -19,24 +25,66 @@ int main(int argc, char *argv[]) {
 	
 	getchar();
 	srand(time(0));
+	system("cls");
 	zahl_summe=0;
-	zahl = rand();
-	zahl = zahl%6+1;
+	error=0;
+	win=0;
+	vorher=0;
+	high=0;
 	
-	do
-	{
-		zahl_summe=zahl_summe+zahl;
-		printf ("Gew""\x81""rfelte Zahl %d \n",zahl);
-		if(zahl_summe>21) {zahl_summe = 0;printf("Verloren, du Opfer!\n"); }
-		if(zahl_summe==0)break;
-		printf("Erreichte Punktzahl bisher: %d\n",zahl_summe);
-		printf("Weiterw""\x81""rfeln ('Enter' um weiter zu spielen ,'c' zum beenden)\n\n");
+	do{	
+		if (high<vorher){
+			high=vorher;
+		}
+		
 		zahl = rand();
 		zahl = zahl%6+1;
-		ja_nein = getch();
-	}while(ja_nein==13);
-	
-	printf("Ergebnis:%d\n",zahl_summe);
-	
+		zahl_summe=zahl_summe+zahl;
+		
+		if(zahl_summe==0){
+			break;
+		}
+		if(zahl_summe>21) {
+			zahl_summe=0;
+			error=error+1;
+		}
+		printf("_______________________________________________________________________________\n");
+		printf("|Gewuerfelte Zahl| %d                                                          \n",zahl);
+		printf("|----------------|-------------------------------------------------------------\n");
+		printf("|Insgesamt       | %d                                                          \n",zahl_summe);
+		printf("|----------------|-------------------------------------------------------------\n");
+		printf("|Hoechste Zahl   | %d                                                          \n",high);
+		printf("|----------------|-------------------------------------------------------------\n");
+		printf("|Erspielte Punkte| %d                                                          \n",win);
+		printf("|----------------|-------------------------------------------------------------\n");
+		printf("|Verloren        | %d                                                          \n",error);
+		printf("|----------------|-------------------------------------------------------------\n");
+		printf("|                |Weiter wuerfeln?[0/1]                        [9 zum beenden] \n");
+		printf("|------------------------------------------------------------------------------\n");
+		scanf("%d",&eingabe);
+		if (eingabe==0){
+			if (high==21){
+				if (high>zahl_summe){
+					error=error+1;
+				}
+				else{
+					win=win+1;
+				}
+			}
+			else{
+				if (high>zahl_summe){
+					error=error+1;
+				}
+				else{
+					win=win+1;
+				}
+			}
+			vorher=zahl_summe;
+			zahl_summe=0;
+		}
+		system("cls");
+		
+		
+	}while (eingabe!=9);
 	return 0;
 }
